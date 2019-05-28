20f2e910854064b778c168d892345f5aaded002d
Batjargal <myracler1@gmail.com>
2019-05-27 21:49:51 -0500

    update hw1

--- Homework_01/Exercise_2/js.js
+++ Homework_01/Exercise_2/js.js
@@ -1,14 +1,33 @@
 
-Array.prototype.removeNum = function(num) {
-    return this.filter(val => val != num);
+Array.prototype.removeNum = async function(num) {
+    try {
+        let myArr = this;
+        let result = await filt(myArr, num);
+        console.log(result);
+        return result;
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+function filt(arr,num) {
+    return new Promise(function(resolve, reject) {
+        if(arr.length != 0) {
+            let newArr = arr.filter(val => val != num);
+            resolve(newArr);
+        }
+        else {
+            reject("no result");
+        }
+    });
 }
 
 let example = [1,2,3,4,5,6,1,1,2,3,4];
 
-
 console.log("start");
 
-console.log(example.removeNum(1));
+example.removeNum(1);
 
 console.log("finish");
 
--- Homework_01/Exercise_3/js.js
+++ Homework_01/Exercise_3/js.js
@@ -2,31 +2,47 @@
 
 const postsPromise = fetch('https://randomuser.me/api');
 
+const { Observable } = rxjs;
+
 function btn1() {
     postsPromise.then(data => data.json())
-    .then(data=> {console.log(data.results[0].name); console.log(data.results[0].location);})
-    .catch((err) =>{console.log(err); })
+        .then(data => { console.log(data.results[0].name); console.log(data.results[0].location); })
+        .catch((err) => { console.log(err); })
 }
 
-
 function btn2() {
-    async function getData() {
+    async function asx() {
         try {
-            console.log("Before await");
+            console.log("before async");
             let data = await prom();
+            console.log(data.results[0].name); console.log(data.results[0].location);
         }
         catch (err) {
-
+            console.log(err);
         }
     }
+    asx();
+    console.log("Later Async");
 }
 
 function btn3() {
-
+    const obs$ = Observable.create(function (observer) {
+       console.log(observer.next());
+    });
 }
 
 function prom() {
-    return new Promise((resolve, reject) => {
-
+    return new Promise(function (resolve, reject) {
+        var xmlHttp = new XMLHttpRequest();
+        let link = 'https://randomuser.me/api';
+        let data = [];
+        xmlHttp.onreadystatechange = function () {
+            if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+                data = JSON.parse(xmlHttp.responseText);
+                resolve(data);
+            }
+        }
+        xmlHttp.open("GET", link);
+        xmlHttp.send();
     });
 }
